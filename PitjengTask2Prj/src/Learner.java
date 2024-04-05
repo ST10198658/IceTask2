276a950663434affc8b5c4a1b594e2d9f7a70bef
RC_Student_lab <RC_Student_lab@10.0.0.155>
2024-04-05 11:30:06 +0000

    create the constuctors

---
+++ PitjengTask2Prj/src/Learner.java
@@ -0,0 +1,25 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author RC_Student_lab
+ */
+public class Learner {
+    private String fname;
+    private String subject;
+    private int assignmentMark;
+    private int testMark;
+    private int examMark;
+    
+    public Learner(){
+    this.fname="";
+    this.subject="";
+    this.assignmentMark=0;
+    this.testMark=0;
+    this.examMark=0;   
+} 
+    
+}
