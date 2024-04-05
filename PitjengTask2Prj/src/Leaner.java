fa75d4eda1eb97c3c886a530360122f4246dc79e
RC_Student_lab <RC_Student_lab@10.0.0.155>
2024-04-05 11:18:52 +0000

    instance variables in the class

---
+++ PitjengTask2Prj/src/Leaner.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author RC_Student_lab
+ */
+public class Leaner {
+    private String fname;
+    private String subject;
+    private int assignmentMark;
+    private int testMark;
+    private int examMark;
+    
+}
