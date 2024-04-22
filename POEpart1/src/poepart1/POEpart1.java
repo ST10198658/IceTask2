12cf4a2bc17fea18bc4fc8f4c5c27e25dbad721e
RC_Student_lab <RC_Student_lab@10.0.0.155>
2024-04-22 13:53:56 +0000

    Create a login Class

---
+++ POEpart1/src/poepart1/POEpart1.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package poepart1;
+
+/**
+ *
+ * @author RC_Student_lab
+ */
+public class POEpart1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Login [] accounts = {};
+        menu(accounts);
+    }
+    
+}
