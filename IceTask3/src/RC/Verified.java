1a6602733b4fa8c5faa3740b3294aaf5e57d34fc
RC_Student_lab <RC_Student_lab@10.0.0.155>
2024-05-21 16:47:45 +0000

    Create a Class call verified AND a method to access Easy Kanban Menu

--- IceTask3/src/RC/Verified.java
+++ IceTask3/src/RC/Verified.java
@@ -4,6 +4,8 @@
  */
 package RC;
 
+import java.util.Scanner;
+
 /**
  *
  * @author RC_Student_lab
@@ -80,5 +82,78 @@ int tasksQauntity = 0;
         newMenu(tasks);
         }else{Verified(tasks);}
         return tasks;
+    }
+    public void newMenu(Task [] tasks){
+         //tasks = p.Populate(tasks);
+        // Populate arrays. (develeoper names, task names, task IDs, task durations, task statuses.)
+        
+        // Display a new menu for when the user has succesfully logged in. (This version of the menu is used once at least one task has been created)
+                Scanner sc = new Scanner(System.in);
+                System.out.println("Welcome to EasyKanban");
+        System.out.println("\nPLEASE SELECT FROM THE FOLLOWING MENU:"
+                + "\n1. Add TASKS."
+                + "\n2. Show Report."
+                + "\n3. DISPLAY TASKS."
+                + "\n4. Search tasks."
+                + "\n5. QUIT.");
+               // Receive user input. 
+        int selection;
+        try {selection = sc.nextInt();
+                }
+        catch (Exception e){
+                selection = Integer.parseInt(sc.nextLine());
+                }
+        
+        switch (selection){
+            case 1:
+
+                System.out.println("How many TASKS would you like to add?");
+                int tasksQUANTITY;
+                tasksQUANTITY = sc.nextInt();
+                
+                Task [] newTasks = new Task[tasks.length + tasksQUANTITY];    
+                
+                for (int i = 0; i < tasks.length; i++){
+                    newTasks [i] = tasks[i];
+                }
+
+                for (int j = 0; j <  tasksQUANTITY;){
+                    System.out.println("Creating task number " + (j + 1));
+                    Task task = new Task();
+                task.Number(tasks);
+                task.Tasker();
+                newTasks[tasks.length + j] =  task;
+                j++;
+                }
+                tasks = newTasks;
+                System.out.println("Tasks ADDED!");
+                System.out.println("Number of task " + (tasks.length));
+                Verified(tasks);
+            break;
+            case 3:
+                System.out.println("DISPLAY TASKS.");
+                displayTask();
+                Verified(tasks);
+            break;
+            case 2:
+
+                System.out.println("Show Report.");
+                for (int i = 0; i < tasks.length; i++){
+                    System.out.println("\n...TASK No: " + "(" + (i+1)+ " of " + tasks.length + ")" + " .........................");
+                tasks[i].showReport();
+                    System.out.println(".........................\n");
+                    
+                }
+                Verified(tasks);
+            break;
+            case 4:
+                System.out.println("Search for task selected.");
+                SearchTasksMenu();
+            case 5:
+                  System.exit(0);
+                break;
+            default:
+            Verified(tasks);    
+        }
     }
 }
