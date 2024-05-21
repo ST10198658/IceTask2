f6681d835825664667c08515a4dac21b16e6caaa
RC_Student_lab <RC_Student_lab@10.0.0.155>
2024-05-21 16:03:11 +0000

    Task number incrementer.

--- IceTask3/src/RC/Task.java
+++ IceTask3/src/RC/Task.java
@@ -4,10 +4,56 @@
  */
 package RC;
 
+import java.util.Scanner;
+
 /**
  *
  * @author RC_Student_lab
  */
 public class Task {
+    Scanner sc = new Scanner(System.in);
+    String taskName, taskDESC, devDETAILS, taskDURATION,taskID,status;
+    int taskNUM, hours, minutes, taskDurationNumerical = (this.hours*60) + this.minutes;
+    
+    public void setTask (String tName, String tID, String taskDuration, String tStatus, String devName) {
+        devDETAILS = devName;
+        taskName = tName;
+        taskDURATION = taskDuration;
+        hours = Integer.parseInt(taskDURATION);
+        status = tStatus;
+        taskID = tID;
     
 }
+    public void Tasker(){
+        System.out.println("ADDING A NEW TASK!"
+                + "\n."
+                + "\n."
+                + "\nTASK NAME: ");
+        nameTask();
+
+        System.out.println("\n."
+                + "\n."
+                + "TASK DESCRIPTION:");
+        this.taskDESC = taskDesc();
+        
+        System.out.println("\n."
+                + "\n."
+                + "DEVELOPER DETAILS:");
+        devDeets();
+        System.out.println(this.devDETAILS);
+        
+        System.out.println("\n."
+                + "\n."
+                + "TASK DURATION:");
+        setDuration();   
+        
+        System.out.println("\n."
+                + "\n."
+                + "TASK IDENTIFICATION NUMBER:");
+        taskID();
+        
+        System.out.println("\n."
+                + "\n."
+                + "TASK STATUS:");
+        taskStatus();
+    } 
