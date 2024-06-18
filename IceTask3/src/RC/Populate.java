f51386c60d4fd5b9796333dbf64cd2c100c4c8d2
RC_Student_lab <RC_Student_lab@10.0.0.155>
2024-06-18 12:21:10 +0000

    Check the array length. If the array already has 4 elements in it move on.

---
+++ IceTask3/src/RC/Populate.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package RC;
+
+/**
+ *
+ * @author RC_Student_lab
+ */
+public class Populate {
+    public Task [] Populate (Task [] tasks){
+    if (tasks.length > 2){
+        return tasks;
+        }
+    }
+}
