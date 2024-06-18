54b768e64353ed3f8ff4dffca4c77cb36b01fec7
RC_Student_lab <RC_Student_lab@10.0.0.155>
2024-06-18 12:32:06 +0000

    Else, populate the "tasks" array with 4 elements

--- IceTask3/src/RC/Populate.java
+++ IceTask3/src/RC/Populate.java
@@ -12,6 +12,19 @@ public class Populate {
     public Task [] Populate (Task [] tasks){
     if (tasks.length > 2){
         return tasks;
-        }
+        }else{
+        Task[] newTasks = new Task[tasks.length + 4];
+
+      newTasks[0] = new Task();
+      newTasks[0].setTask("Create Login", "0001", "5", "To Do", "Mike Smith");
+
+      newTasks[1] = new Task();
+      newTasks[1].setTask("Create Add Features", "0002", "8", "Doing", "Edward Harrison");
+
+      newTasks[2] = new Task();
+      newTasks[2].setTask("Create Report", "0003", "2", "Done", "Samantha Paulson");
+
+      newTasks[3] = new Task();
+      newTasks[3].setTask("Add Arrays", "0004", "11", "To Do", "Glenda Oberholzer");
     }
 }
